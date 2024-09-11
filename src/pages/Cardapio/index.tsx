2a0cad2587e2f6a677f613829c257732e962c16c
biapagotto <biapagotto.ux@gmail.com>
2024-09-11 14:38:50 -0300

    banner atualização

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../components/Header'
 import Hero from '../../components/Hero'
 import MenuList, { MenuItem } from '../../components/MenuList'
@@ -9,21 +9,39 @@ import { useSelector, useDispatch } from 'react-redux'
 import { RootState, AppDispatch } from '../../Redux/store'
 import { addToCart, removeFromCart } from '../../Redux/cartSlice'
 
-const Cardapio = () => {
+const Cardapio: React.FC = () => {
   const { id } = useParams<{ id: string }>()
   const [menuItems, setMenuItems] = useState<MenuItem[]>([])
   const [selectedProduct, setSelectedProduct] = useState<MenuItem | null>(null)
+  const [restaurantInfo, setRestaurantInfo] = useState<{
+    capa: string
+    titulo: string
+    tipo: string
+  } | null>(null)
   const [showCart, setShowCart] = useState(false)
 
   const dispatch = useDispatch<AppDispatch>()
   const cartItems = useSelector((state: RootState) => state.cart.items)
 
   useEffect(() => {
-    fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`)
-      .then((res) => res.json())
-      .then((res) => {
-        setMenuItems(res.cardapio)
-      })
+    const fetchRestaurantData = async () => {
+      try {
+        const response = await fetch(
+          `https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`
+        )
+        const data = await response.json()
+        setMenuItems(data.cardapio)
+        setRestaurantInfo({
+          capa: data.capa,
+          titulo: data.titulo,
+          tipo: data.tipo
+        })
+      } catch (error) {
+        console.error('Erro ao buscar dados do restaurante:', error)
+      }
+    }
+
+    fetchRestaurantData()
   }, [id])
 
   const handleOpenModal = (product: MenuItem) => {
@@ -55,7 +73,13 @@ const Cardapio = () => {
   return (
     <>
       <Header onCartClick={handleToggleCart} cartItemCount={cartItems.length} />
-      <Hero />
+      {restaurantInfo && (
+        <Hero
+          titulo={restaurantInfo.titulo}
+          tipo={restaurantInfo.tipo}
+          capa={restaurantInfo.capa}
+        />
+      )}
       <MenuList items={menuItems} onItemClick={handleOpenModal} />
       {selectedProduct && (
         <ModalCompra
