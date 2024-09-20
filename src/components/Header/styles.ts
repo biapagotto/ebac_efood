28b9ed4ef32c1ac2a6635eddbcbf4bfb6a91c2cd
biapagotto <biapagotto.ux@gmail.com>
2024-09-20 11:51:24 -0300

    Correção

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -10,6 +10,7 @@ export const HeaderContainer = styled.header`
   justify-content: space-between;
   padding: 0 20px;
   overflow: hidden;
+  z-index: 1;
 `
 
 export const BackgroundImage = styled.img`
@@ -19,31 +20,30 @@ export const BackgroundImage = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
-  z-index: 1;
+  z-index: 2;
 `
 
 export const Logo = styled.img`
   width: 125px;
   height: 57.5px;
-  z-index: 2;
   cursor: pointer;
   position: relative;
-  margin-right: px;
+  z-index: 2;
 `
 
 export const RestaurantsText = styled.div`
   font-size: 18px;
   font-weight: 900;
   color: ${cores.coral};
-  z-index: 2;
   position: relative;
   text-decoration: none;
+  z-index: 2;
 `
 
 export const CartText = styled.div`
   font-size: 18px;
   font-weight: 900;
   color: ${cores.coral};
-  z-index: 2;
   position: relative;
+  z-index: 2;
 `
